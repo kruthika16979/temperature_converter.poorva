af290b4736a7fac1ca5b74d3cb5aa7900726c99e
poorva2409 <poorva.k@s.amity.edu>
2025-07-06 17:59:32 +0530

    Handled invalid choice

--- temp.c
+++ temp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int choice;
     float temp;
 
@@ -10,16 +11,22 @@ int main() {
     printf("Enter your choice: ");
     scanf("%d", &choice);
 
-    if (choice == 1) {
+    if (choice == 1) 
+{
     printf("Enter temperature in Celsius: ");
     scanf("%f", &temp);
     printf("Temperature in Fahrenheit: %.2f\n", (temp * 9 / 5) + 32);
 }
 
-    else if (choice == 2) {
+    else if (choice == 2)
+{
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &temp);
     printf("Temperature in Celsius: %.2f\n", (temp - 32) * 5 / 9);
+}
+    else 
+{
+    printf("Invalid choice.\n");
 }
     return 0;
 }
