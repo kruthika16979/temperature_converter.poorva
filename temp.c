e9003f8dd76f26c33596e3324af101a7186fd819
poorva2409 <poorva.k@s.amity.edu>
2025-07-06 17:57:21 +0530

    Added Fahrenheit to Celsius conversion

--- temp.c
+++ temp.c
@@ -9,11 +9,17 @@ int main() {
     printf("2. Fahrenheit to Celsius\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
+
     if (choice == 1) {
     printf("Enter temperature in Celsius: ");
     scanf("%f", &temp);
     printf("Temperature in Fahrenheit: %.2f\n", (temp * 9 / 5) + 32);
 }
 
+    else if (choice == 2) {
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &temp);
+    printf("Temperature in Celsius: %.2f\n", (temp - 32) * 5 / 9);
+}
     return 0;
 }
