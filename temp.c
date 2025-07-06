6c48feb444d5aab6ec59f9898ae5f01faf4eea91
poorva2409 <poorva.k@s.amity.edu>
2025-07-06 17:52:50 +0530

    Added menu setup

---
+++ temp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float temp;
+
+    printf("Temperature Converter\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    return 0;
+}
